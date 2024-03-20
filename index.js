4fdcc179dc934b2ea27fec626d59cd16856b4ea1
mfwolfiee <131528039+mfwolfiee@users.noreply.github.com>
2024-03-20 19:06:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('star')
     .setDetails(`༺♰༻ [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('mp:attachments/991719725019181076/1219880765844750367/346.gif?ex=660ce97e&is=65fa747e&hm=ab61035fbeb4f599fb5f580edbfba293196c897239e35b06df6c4c43d37c909f&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('mp:attachments/991719725019181076/1219965196869701653/1.gif?ex=660d3820&is=65fac320&hm=ec44d5c440e5a8977d0dd2113096cf3d9f7e173e11360bbd53da8c428bb50115&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('lover') //Text when you hover the Large image
     .setAssetsSmallImage('mp:attachments/991719725019181076/1219881541920886875/1122166223774417017.gif?ex=660cea37&is=65fa7537&hm=55c333b8b110e960ddccd63a1a15cab3634d87bf1f19fedaa0edeaab440c556b&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('lost') //Text when you hover the Small image
