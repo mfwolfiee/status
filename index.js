de21cc0f2cd3bfc9f6da47146007e049cfae9d4a
mfwolfiee <131528039+mfwolfiee@users.noreply.github.com>
2024-04-12 07:13:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `blood`;
+      const newDetails = `prairie`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
